7bcc52f06d2073181a392460459416db22ee2fbd
Ian German Mesner <ian@chartiq.com>
2018-05-10 11:57:48 -0400

    6954 more redux actions comments

--- src/actions/studyActions.js
+++ src/actions/studyActions.js
@@ -1,3 +1,8 @@
+/**
+ * Drawing actions for redux actions involved with the studies on the chart
+ * @module actions/studyActions
+ */
+
 import createTypes from 'redux-create-action-types'
 
 /*
@@ -15,34 +20,81 @@ const Types = createTypes(
 
 export default Types
 
-/*
- * action creators
+/**
+ * Assign the study helper to the charting engine
+ *
+ * @export
+ * @param {any} helper
+ * @returns
  */
-
 export function setStudyHelper(helper){
     return { type: 'SET_STUDY_HELPER', helper: helper }
 }
 
+/**
+ * Show or hide the study overlay
+ *
+ * @export
+ * @param {any} params
+ * @returns
+ */
 export function toggleOverlay(params){
     return { type: 'TOGGLE_STUDY_OVERLAY', params: params }
 }
 
+/**
+ * Open study configuration window
+ *
+ * @export
+ * @param {any} params
+ * @returns
+ */
 export function openStudyModal(params){
     return { type: 'OPEN_STUDY_MODAL', params: params }
 }
 
+/**
+ * Close study configuration window
+ *
+ * @export
+ * @returns
+ */
 export function closeStudyModal(){
     return { type: 'CLOSE_STUDY_MODAL' }
 }
 
+/**
+ * Adds a study to the chart
+ *
+ * @export
+ * @param {CIQ.ChartEngine} ciq
+ * @param {String} study
+ * @returns
+ */
 export function addStudy(ciq, study){
     return { type: 'ADD_STUDY', ciq: ciq, study: study }
 }
 
+/**
+ * Update study configuration
+ *
+ * @export
+ * @param {any} inputs
+ * @param {any} outputs
+ * @param {any} parameters
+ * @returns
+ */
 export function updateStudy(inputs, outputs, parameters){
     return { type: 'UPDATE_STUDY', inputs: inputs, outputs: outputs, parameters: parameters }
 }
 
+/**
+ * Remove study from chart
+ *
+ * @export
+ * @param {String} study name of study
+ * @returns
+ */
 export function removeStudy(study){
     return { type: 'REMOVE_STUDY', study: study }
 }
