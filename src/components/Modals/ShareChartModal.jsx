715f386cc21400153b8be10f796a66515d7f8fc4
normunds-ciq <normunds@chartiq.com>
2020-05-21 23:23:38 -0400

    Correct import path

--- src/components/Modals/ShareChartModal.jsx
+++ src/components/Modals/ShareChartModal.jsx
@@ -4,8 +4,7 @@
  */
 
 import React from 'react';
-import { CIQ } from '../../../../chartiq/js/chartiq';
-
+import { CIQ } from '../../../chartiq/js/chartiq';
 /**
  *  ShareStatus enumeration
  */
--- src/components/Modals/TimezoneModal.jsx
+++ src/components/Modals/TimezoneModal.jsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { CIQ } from '../../../../chartiq/js/chartiq';
+import { CIQ } from '../../../chartiq/js/chartiq';
 
 /**
  * Timezone selection modal dialog window component
